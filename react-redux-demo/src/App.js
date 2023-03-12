bc38e8197eee3e8332e7220c3d1fae5c3a1e920a
josue josuerushanika <100031798+josuerushanika@users.noreply.github.com>
2023-03-13 00:44:34 +0200

    Adding react Provider

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+// eslint-disable-next-line
+
+import React from 'react';
+import { Provider } from 'react';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
 
 function App() {
   return (
+    <Provider>
     <div className="App">
       <CakeContainer />
     </div>
+    <Provider/>
   );
 }
 
---
+++ react-redux-demo/src/redux/store.js
@@ -0,0 +1,5 @@
+import {createStore} from 'redux'
+import CakeReducer from './cake/cakeReducer'
+const store = createStore(CakeReducer)
+
+export default store
